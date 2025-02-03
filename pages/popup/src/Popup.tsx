81aa7456ea928343da87871d1e0388f7c875d7e0
Iliya Brook <iliyabrook1987@gmail.com>
2025-02-03 17:37:43 +0200

    rm side panel

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -14,40 +14,17 @@ const Popup = () => {
   const theme = useStorage(themeStorage);
   const isLight = theme === 'light';
 
-  const injectContentScript = async () => {
-    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    if (tab.url!.startsWith('about:') || tab.url!.startsWith('chrome:')) {
-      chrome.notifications.create('inject-error', notificationOptions);
-    }
-
-    await chrome.scripting
-      .executeScript({
-        target: { tabId: tab.id! },
-        files: ['/content-runtime/index.iife.js'],
-      })
-      .catch(err => {
-        // Handling errors related to other paths
-        if (err.message.includes('Cannot access a chrome:// URL')) {
-          chrome.notifications.create('inject-error', notificationOptions);
-        }
-      });
-  };
-
   return (
     <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
       <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        <p>
-          Edit <code>pages/popup/src/Popup.tsx</code>
-        </p>
-        <button
-          className={
-            'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-            (isLight ? 'bg-blue-200 text-black' : 'bg-gray-700 text-white')
-          }
-          onClick={injectContentScript}>
-          Click to inject Content Script
-        </button>
+        {/* <button */}
+        {/*   className={ */}
+        {/*     'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' + */}
+        {/*     (isLight ? 'bg-blue-200 text-black' : 'bg-gray-700 text-white') */}
+        {/*   } */}
+        {/*   onClick={injectContentScript}> */}
+        {/*   Click to inject Content Script */}
+        {/* </button> */}
         <ToggleButton>Toggle theme</ToggleButton>
       </header>
     </div>
